2252b6e5e5a1bc2cfea0e851fdf011f1ee4bba65
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-11-21 10:07:11 -0700

    finished JSX of SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,7 +15,30 @@ class SearchBar extends Component {
             let sortByOptionValue = sortByOptions[sortByOption]
             return <li key={sortByOptionValue}> {sortByOption} </li>;
         })
-        
+     
+    }
+    render () {
+        return (
+            
+
+            <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+              <ul>
+                <!-- Use .renderSortByOptions() to sort the businesses by their options -->
+              </ul>
+            </div>
+            <div className="SearchBar-fields">
+              <input placeholder="Search Businesses" />
+              <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+              <a>Let's Go</a>
+            </div>
+          </div>
+
+
+           
+        )
     }
 }
 
