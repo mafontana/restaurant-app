4a51a4fe33e6797650fb1d412d85731de12350e6
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-11-21 09:44:45 -0700

    added searchByOptions object to SearchBar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count"
+
+}
+
+class SearchBar extends Component {
+
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption]
+            return <li key={sortByOptionValue}> {sortByOption} </li>;
+        })
+        
+    }
+}
+
+export default SearchBar
