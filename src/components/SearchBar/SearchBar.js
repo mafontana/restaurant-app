b8da4aee65790acd3fec8429a30ae5b0a011f8ec
Meredith Fontana <meredith@Merediths-MBP.Home>
2018-11-22 12:20:20 -0700

    added to renderSortByOptions method so that sorting options are highlighted for user

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -36,7 +36,11 @@ class SearchBar extends Component {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption]
-            return <li key={sortByOptionValue}> {sortByOption} </li>;
+            return (<li
+                key={sortByOptionValue}
+                className={this.getSortByClass(sortByOptionValue)}
+                onClick={this.handleSortByChange.bind(this,sortByOptionValue)}>
+                  {sortByOption} </li>);
         })
      
     }
