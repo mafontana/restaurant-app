705159ae4abd78b73dcecfbbbd1658b4c9da44be
Meredith Fontana <meredith@Merediths-MBP.Home>
2018-11-22 17:15:59 -0700

    added searchYelp method to App.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -17,6 +17,9 @@ class SearchBar extends Component {
             location: "", 
             sortBy: 'best_match'
         }
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        
     }
 
     getSortByClass (sortByOption) {
@@ -65,8 +68,8 @@ class SearchBar extends Component {
               </ul>
             </div>
             <div className="SearchBar-fields">
-              <input placeholder="Search Businesses" />
-              <input placeholder="Where?" />
+              <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+              <input placeholder="Where?" onChange={this.handleLocationChange}/>
             </div>
             <div className="SearchBar-submit">
               <a>Let's Go</a>
